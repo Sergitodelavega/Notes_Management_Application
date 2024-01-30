dfaeef1faae8fa1e0145488f12ff825f75a754fa
Sergio Lissanou <79195821+Sergitodelavega@users.noreply.github.com>
2024-01-30 18:50:43 +0100

    Home Page and Authentication

---
+++ resources/views/front/partials/header.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg navbar-light py-3 px-5 bg-primary">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="javascript:void(0)">Navbar</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="javascript:void(0)">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="javascript:void(0)">Link</a>
+          </li>
+        </ul>
+        <div>
+          @guest
+            <a href="{{ route('register') }}" class="btn rounded-pill me-2 btn-outline-warning">S'inscrire</a>
+            <a href="{{ route('login') }}" class="btn rounded-pill me-2 btn-warning">Se connecter</a>
+          @else
+          <form class="d-flex" action="javascript:void(0)">
+            <button type="button" class="btn rounded-pill me-2 btn-warning">S'inscrire'</button>
+          </form>
+          @endguest
+        </div>
+      </div>
+    </div>
+  </nav>
---
+++ resources/views/front/partials/style.blade.php
@@ -0,0 +1,34 @@
+  <!-- Favicon -->
+  <link rel="icon" type="image/x-icon" href="{{ asset('appweb/assets/img/favicon/favicon.ico') }}" />
+
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+    rel="stylesheet">
+  <link
+    href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap"
+    rel="stylesheet"/>
+
+  <!-- Icons. Uncomment required icon fonts -->
+  <link rel="stylesheet" href="{{ asset('appweb/assets/vendor/fonts/boxicons.css') }}" />
+
+    <!-- Core CSS -->
+    <link rel="stylesheet" href="{{ asset('appweb/assets/vendor/css/core.css') }}" />
+    <link rel="stylesheet" href="{{ asset('appweb/assets/vendor/css/theme-default.css') }}" />
+    <link rel="stylesheet" href="{{ asset('appweb/assets/css/demo.css') }}" />
+
+  <!-- Vendors CSS -->
+  <link rel="stylesheet" href="{{ asset('appweb/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar.css') }}" />
+
+  <!-- Page CSS -->
+  <!-- Page -->
+  <link rel="stylesheet" href="{{ asset('appweb/assets/vendor/css/pages/page-auth.css') }}" />
+
+  <!-- Helpers -->
+  <script src="{{ asset('appweb/assets/vendor/js/helpers.js') }}"></script>
+
+  <!--! Template customizer & Theme config files MUST be included after core stylesheets and helpers.js in the <head> section -->
+  <!--? Config:  Mandatory theme config file contain global vars & default theme options, Set your preferred theme option in this file.  -->
+  <script src="{{ asset('appweb/assets/js/config.js') }}"></script>
